aca17bf78ab323b4dde6d7a941704c336156143a
nikania <nianemis@gmail.com>
2021-03-25 20:25:28 +0300

    add folders

---
+++ SampleOrderService/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SampleOrderService.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleOrderService.Controllers
+{
+    public class BaseController<T> : ControllerBase where T : IService
+    {
+
+    }
+}
